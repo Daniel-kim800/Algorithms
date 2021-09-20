fe57ab17069623fce7eadf7dc411cca742571663
Daniel-kim800 <55107493+Daniel-kim800@users.noreply.github.com>
2021-09-19 20:25:06 -0500

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,11 +1,9 @@
 #get last 5 digits of nth fibonacci number
 def Last5Fib(num):
-  x=0
-  y=0
-  z=1
+  x,y,z=0,0,1
   for i in range(2,num+1):
     x=y
     y=z
     z=(x+y)%10000
   return z
-print(Last5Fib(999999999))
+print(Last5Fib(99999999))
